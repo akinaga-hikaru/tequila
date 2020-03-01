ed62f1129110bb523137f506be62794d71acc0b9
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-03-01 18:15:55 +0900

    表示修正（商品紹介ページ）

--- resources/views/tequila/component/syouhin_parts.blade.php
+++ resources/views/tequila/component/syouhin_parts.blade.php
@@ -6,21 +6,21 @@
     </p>
     <p class="letters-area">
         <strong class="letters-title">{{ $aging }}</strong><br>
-        ■ ALC：{!! $contents_alc !!}<br>
-        ■ DEST：{!! $contents_dest !!}<br>
-        ■ DEST.NUM：NOM {!! $contents_nom !!}<br>
-        ■ AREA：{!! $contents_area !!}<br>
+        ■ 度数：{!! $contents_alc !!}<br>
+        ■ 蒸留所：{!! $contents_dest !!}<br>
+        ■ 蒸留所番号：NOM {!! $contents_nom !!}<br>
+        ■ 生産地域：{!! $contents_area !!}<br>
         @if (!empty($contents_info))
-            ■ INFO：{!! $contents_info !!}<br>
+            ■ 情報：{!! $contents_info !!}<br>
         @endif
         @if (!empty($contents_review_flavor))
-            ■ FLAVOR：{!! $contents_review_flavor !!}<br>
+            ■ 香り：{!! $contents_review_flavor !!}<br>
         @endif
         @if (!empty($contents_review_top))
-            ■ TOP：{!! $contents_review_top !!}<br>
+            ■ 味：{!! $contents_review_top !!}<br>
         @endif
         @if (!empty($contents_review_after))
-            ■ AFTER：{!! $contents_review_after !!}
+            ■ 余韻：{!! $contents_review_after !!}
         @endif
     </p>
 </div>
