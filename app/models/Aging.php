6fa02fc412026cd191c5ce84726cc8e3dd6de299
moon-gm <yoakemae256@gmail.com>
2021-04-14 11:53:24 +0900

    各テーブルのモデルを作成

---
+++ app/models/Aging.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aging extends Model
+{
+    protected $table = 'agings';
+
+}
---
+++ app/models/Destilador.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destilador extends Model
+{
+    protected $table = 'destiladors';
+
+}
