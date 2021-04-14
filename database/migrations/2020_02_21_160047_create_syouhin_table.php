b4c8d1f02eaf79099ae1ccfc24f8022830b33354
moon-gm <yoakemae256@gmail.com>
2021-04-14 11:56:36 +0900

    syouhinテーブルに挿入するデータをマスタから取得するように修正/画像名修正

--- database/migrations/2020_02_21_160047_create_syouhin_table.php
+++ database/migrations/2020_02_21_160047_create_syouhin_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
-use App\SyouhinTable;
 
 class CreateSyouhinTable extends Migration
 {
@@ -21,10 +20,10 @@ class CreateSyouhinTable extends Migration
             $table->string('title_id')->comment('ブランド名ID');
             $table->string('title_name')->comment('ブランド名');
             $table->string('alt_name')->comment('種類');
-            $table->string('url')->comment('商品の外部URL');
             $table->string('image')->comment('イメージ画像');
             $table->string('aging_sort')->comment('熟成度合い大分類');
             $table->string('aging')->comment('熟成名称');
+            $table->string('url')->comment('商品の外部URL');
             $table->string('contents_alc')->comment('度数');
             $table->string('contents_dest')->comment('蒸留所');
             $table->string('contents_nom')->comment('蒸留所番号');
@@ -38,36 +37,6 @@ class CreateSyouhinTable extends Migration
             $table->string('contents_review_after')->comment('レビュー：余韻');
             $table->timestamps();
         });
-
-        // データに挿入する値の定義
-        $syouhin = SyouhinTable::$syouhin_table;
-
-        // データを初期化
-        DB::table('syouhin')->truncate();
-
-        // テーブルにデータを挿入
-        foreach ($syouhin as $each_syouhin) {
-            DB::table('syouhin')->insert([
-                'title_id' => $each_syouhin['title_id'],
-                'title_name' => $each_syouhin['title_name'],
-                'alt_name' => $each_syouhin['alt_name'],
-                'url' => $each_syouhin['url'],
-                'image' => $each_syouhin['image'],
-                'aging_sort' => $each_syouhin['aging_sort'],
-                'aging' => $each_syouhin['aging'],
-                'contents_alc' => $each_syouhin['contents_alc'],
-                'contents_dest' => $each_syouhin['contents_dest'],
-                'contents_nom' => $each_syouhin['contents_nom'],
-                'contents_local_id' => $each_syouhin['contents_local_id'],
-                'contents_local' => $each_syouhin['contents_local'],
-                'contents_area_id' => $each_syouhin['contents_area_id'],
-                'contents_area' => $each_syouhin['contents_area'],
-                'contents_info' => $each_syouhin['contents_info'],
-                'contents_review_flavor' => $each_syouhin['contents_review_flavor'],
-                'contents_review_top' => $each_syouhin['contents_review_top'],
-                'contents_review_after' => $each_syouhin['contents_review_after'],
-            ]);
-        }
     }
 
     /**
