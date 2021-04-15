726258d1e6e2f18419ea6fb9b338260cb8d39408
moon-gm <yoakemae256@gmail.com>
2021-04-15 23:53:52 +0900

    GoogleAnalytics追加

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BEG7SRZCCN"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BEG7SRZCCN');
+        </script>
         <meta charset="UTF-8">
         <meta name="description" content={{ config('app_layout.meta.description') }}>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
